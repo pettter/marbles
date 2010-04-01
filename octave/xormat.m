c07f76586f21e1d0877c8118fca0e6c0175cac26
pettter <pettter@68f39699-0fb3-c15f-b0dd-e6705a8e7579>
2010-04-01 14:49:39 +0000

    Fiddle around with xor math in octave for article
    git-svn-id: svn://svn.cs.umu.se/2010/exjobb/pettter_drewes@9 68f39699-0fb3-c15f-b0dd-e6705a8e7579

---
+++ octave/xormat.m
@@ -0,0 +1,68 @@
+function r = xorify(A)
+	r = mod(A,2)
+end
+
+function r = mul(A,B)
+	r = xorify(A * B);
+end
+
+function r = add(A,B)
+	r = xorify(A + B);
+end
+
+function P = getP(it,jt,v)
+	P = eye(length(v));
+	for ix = 1 : length(v)
+		if(ix != jt)
+			P(:,ix) = add(P(:,ix),mul(v(ix),P(:,jt)));
+		end
+	end
+	t = P(:,it);
+	P(:,it) = P(:,jt);
+	P(:,jt) = t;
+end
+
+function [Ir,Ar,Fr] = Simi(it,jt,v,I,A,F)
+	P = getP(it,jt,v);
+	Pi = xorify(inv(P));
+	for ix = 1:length(A(:,1,1))
+		Ar(ix,:,;) = mul(mul(Pi,A(ix,:,:)),P);
+	end
+	Ir = mul(I,P);
+	Fr = mul(Pi,F);
+end
+
+function ix = lastzero(v)
+	for ix = length(v)-1:-1:0
+		if(v(ix))
+			return
+		end
+	end
+end
+
+function [I,A,F,d] = lacc(I,A,F)
+	if(sum(I) == 0)
+		I=[]
+		A = []
+		F=[]
+		return
+	end
+	a = 1; b = 1;
+	jt = lastzero(I);
+	[I,A,F] = Simi(b,jt,I,I,A,F); b++;
+	while (a < b)
+		q = a++;
+	    for ix = 1:length(A(:,1,1))
+			v = A(ix,q,:);
+			jt = lastzero(v);
+			if(jt > b)
+				[I,A,F] = Simi(b,jt,v,I,A,F);
+			end
+		end
+	end
+	I = I(1:b)
+	F = F(1:b)
+	A(:) = A(:,1:b,1:b)
+end
+
+
